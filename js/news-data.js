25a57f93a7ca8a544926a713a09307298e4b81e6
himanoyosuga <296430306+himanoyosuga@users.noreply.github.com>
2026-06-28 17:30:16 +0900

    6/28 fixed

--- js/news-data.js
+++ js/news-data.js
@@ -1,7 +1,14 @@
 const newsItems = [
   {
+    date: "2026.06.29",
+    title: "DOG RULE 定期開催決定のお知らせ",
+    url: "./news/2026-06-29.html"
+  },
+  
+  {
     date: "2026.06.14",
     title: "DOG RULE 開催のお知らせ",
-    url: "./news/2026-06-14-open.html"
+    url: "./news/2026-06-14.html"
   },
+
 ];
